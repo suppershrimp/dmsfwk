1173fe8efb6a158080544c07fdebaa538a26a441
fengsy <fengsy0712@thundersoft.com>
2022-03-28 22:06:36 +0800

    add continue for freeinstall
    Signed-off-by: fengsy <fengsy0712@thundersoft.com>

---
+++ services/dtbschedmgr/include/dms_callback_session.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_DMS_CALLBACK_SESSION_H
+#define OHOS_DMS_CALLBACK_SESSION_H
+
+#include <cstdint>
+#include <map>
+#include <mutex>
+
+#include "event_handler.h"
+#include "iremote_object.h"
+#include "refbase.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+using DmsCallbackSessionInitCallbackFunc = std::function<void(int32_t sessionId)>;
+
+struct CallbackSessionItem {
+    sptr<IRemoteObject> callback;
+    int32_t sessionId = -1;
+    bool isContinue = false;
+};
+
+class DmsCallbackSession : public std::enable_shared_from_this<DmsCallbackSession> {
+public:
+    void Init(const DmsCallbackSessionInitCallbackFunc& callback);
+    int32_t GenerateSessionId();
+    bool PushCallback(int32_t sessionId, const sptr<IRemoteObject>& callback, bool isContinue);
+    sptr<IRemoteObject> PopCallback(int32_t sessionId);
+    void SetContinuationSessionMap(int32_t freeInstallSessionId, int32_t continueSessionId);
+    bool GetContinueFlag(int32_t sessionId);
+    int32_t GetContinuaionSessionId(int32_t sessionId);
+
+private:
+    class DmsCallbackHandler : public AppExecFwk::EventHandler {
+    public:
+        DmsCallbackHandler(const std::shared_ptr<AppExecFwk::EventRunner>& runner,
+            const std::shared_ptr<DmsCallbackSession>& callbackSession,
+            const DmsCallbackSessionInitCallbackFunc& callback)
+            : AppExecFwk::EventHandler(runner), callbackSession_(callbackSession), callback_(callback) {}
+        ~DmsCallbackHandler() = default;
+
+    private:
+        std::weak_ptr<DmsCallbackSession> callbackSession_;
+        DmsCallbackSessionInitCallbackFunc callback_;
+    };
+
+    std::shared_ptr<DmsCallbackHandler> dmsCallbackHandler_;
+    std::mutex callbackMapMutex_;
+    std::mutex sessionMutex_;
+    int32_t currSessionId_ = 1;
+    std::map<int32_t, CallbackSessionItem> callbackMap_;
+    std::map<int32_t, int32_t> continuationSessionMap_;
+};
+}  // namespace DistributedSchedule
+}  // namespace OHOS
+#endif  // OHOS_DMS_CALLBACK_SESSION_H
---
+++ services/dtbschedmgr/src/dms_callback_session.cpp
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dms_callback_session.h"
+
+#include "dtbschedmgr_log.h"
+#include "parcel_helper.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+using namespace OHOS::AppExecFwk;
+
+namespace {
+constexpr int64_t Callback_DELAY_TIME = 20000;
+const std::string TAG = "DmsCallbackSession";
+}
+
+void DmsCallbackSession::Init(const DmsCallbackSessionInitCallbackFunc& callback)
+{
+    auto runner = EventRunner::Create("dsched_callback");
+    dmsCallbackHandler_ = std::make_shared<DmsCallbackHandler>(runner, shared_from_this(), callback);
+}
+
+int32_t DmsCallbackSession::GenerateSessionId()
+{
+    std::lock_guard<std::mutex> autoLock(sessionMutex_);
+    int32_t currValue = currSessionId_;
+    if (++currSessionId_ <= 0) {
+        currSessionId_ = 1;
+    }
+    return currValue;
+}
+
+bool DmsCallbackSession::PushCallback(int32_t sessionId, const sptr<IRemoteObject>& callback, bool isContinue)
+{
+    if (sessionId <= 0) {
+        HILOGE("PushCallback sessionId invalid!");
+        return false;
+    }
+
+    if (callback == nullptr) {
+        HILOGE("PushCallback callback null!");
+        return false;
+    }
+
+    if (dmsCallbackHandler_ == nullptr) {
+        HILOGE("PushCallback not initialized!");
+        return false;
+    }
+
+    bool ret = dmsCallbackHandler_->SendEvent(sessionId, 0, Callback_DELAY_TIME);
+    if (!ret) {
+        HILOGE("PushCallback SendEvent failed!");
+        return false;
+    }
+
+    std::lock_guard<std::mutex> autoLock(callbackMapMutex_);
+    auto iterSession = callbackMap_.find(sessionId);
+    if (iterSession != callbackMap_.end()) {
+        HILOGE("PushCallback sessionId:%{public}d exist!", sessionId);
+        return false;
+    }
+
+    CallbackSessionItem item {callback, sessionId, isContinue};
+    (void)callbackMap_.emplace(sessionId, item);
+    return true;
+}
+
+sptr<IRemoteObject> DmsCallbackSession::PopCallback(int32_t sessionId)
+{
+    std::lock_guard<std::mutex> autoLock(callbackMapMutex_);
+    auto iter = callbackMap_.find(sessionId);
+    if (iter == callbackMap_.end()) {
+        HILOGW("PopCallback not found sessionId:%{public}d", sessionId);
+        return nullptr;
+    }
+
+    CallbackSessionItem item = iter->second;
+    // sptr<IRemoteObject> callback = iter->second;
+    (void)callbackMap_.erase(iter);
+    if (dmsCallbackHandler_ != nullptr) {
+        dmsCallbackHandler_->RemoveEvent(sessionId);
+    }
+
+    return item.callback;
+}
+
+int32_t DmsCallbackSession::GetContinuaionSessionId(int32_t freeInstallSessionId)
+{
+    if (freeInstallSessionId <= 0) {
+        return INVALID_PARAMETERS_ERR;
+    }
+
+    std::lock_guard<std::mutex> autoLock(callbackMapMutex_);
+    auto iter = continuationSessionMap_.find(freeInstallSessionId);
+    if (iter != continuationSessionMap_.end()) {
+        return INVALID_PARAMETERS_ERR;
+    }
+    return iter->second;
+}
+
+void DmsCallbackSession::SetContinuationSessionMap(int32_t freeInstallSessionId, int32_t continueSessionId)
+{
+    if (freeInstallSessionId <= 0 || continueSessionId <= 0) {
+        HILOGD("param invalid");
+        return;
+    }
+    std::lock_guard<std::mutex> autoLock(callbackMapMutex_);
+    auto itreator = continuationSessionMap_.find(freeInstallSessionId);
+    if (itreator != continuationSessionMap_.end()) {
+        HILOGD("freeInstallSessionId = %{public}d", freeInstallSessionId);
+        return;
+    }
+    continuationSessionMap_[freeInstallSessionId] = continueSessionId;
+}
+
+bool DmsCallbackSession::GetContinueFlag(int32_t sessionId)
+{
+    if (sessionId <= 0) {
+        HILOGD("GetContinueFlag param sessionId invalid");
+        return false;
+    }
+    std::lock_guard<std::mutex> autoLock(callbackMapMutex_);
+    auto iterSession = callbackMap_.find(sessionId);
+    if (iterSession == callbackMap_.end()) {
+        HILOGE("GetContinueFlag not found sessionId : %{public}d", sessionId);
+        return false;
+    }
+    CallbackSessionItem item = iterSession->second;
+    return item.isContinue;
+}
+}  // namespace DistributedSchedule
+}  // namespace OHOS
