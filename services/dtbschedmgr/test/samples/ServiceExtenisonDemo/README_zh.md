e2e603376e8e1ce876bedd79603cd8fe753e64bc
wangyang2022 <wangyang412@huawei.com>
2022-07-26 10:14:58 +0800

    跨设备绑定 ServiceExtensionAbility 示例 Demo Signed-off-by: wangyang2022 <wangyang412@huawei.com> Change-Id: Ida0e6a48103ae9e18e9c6bf1542f17ee4aa433cf

---
+++ services/dtbschedmgr/test/samples/ServiceExtenisonDemo/README_zh.md
@@ -0,0 +1,29 @@
+# 跨设备绑定ServiceExtensionAbility示例
+
+### 简介
+本示例展示了在eTS中如何构建stage模型跨设备绑定ServiceExtensionAbility，包含:
+
+1、远端实现待绑定 ServiceExtensionAbility
+
+2、申请分布式权限：DISTRIBUTED_DATASYNC
+
+3、发起端调用 context.connectAbility() 接口，传入 deviceId，bundleName，abilityName 绑定远端 ServiceExtensionAbility
+
+4、绑定成功获取远端 ServiceExtensionAbility 句柄，发起段使用句柄的 sendRequest() 接口调用远端定义的方法
+
+
+### 测试步骤
+1、两台设备都安装此应用，并组网成功（计算器可以跨端拉起）
+
+2、在设置里给予此应用分布式权限
+
+3、设备 A 打开应用，选择待绑定设备 B 的 deviceId，点击 connect 按钮发起绑定
+
+4、设备 A 日志区显示 “get proxy”，代表绑定成功
+
+5、设备 A 在输入栏输入任意字符串，点击 send 按钮，信息被发送到远端设备 B
+
+6、设备 A 收到设备 B 的回复消息 “MyServiceExtensionAbility receive your message”
+
+### 约束与限制
+1、本示例仅支持标准系统上运行。 2、本示例为Stage模型，仅支持API version 9，IDE版本3.0.1.900以上，系统版本OpenHarmony3.1 release。
---
+++ services/dtbschedmgr/test/samples/ServiceExtenisonDemo/entry/src/main/ets/MyServiceExtensionAbility/MyServiceExtensionAbility.ts
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// @ts-nocheck
+
+import rpc from '@ohos.rpc';
+import ServiceExtensionAbility from '@ohos.application.ServiceExtensionAbility';
+var printLog = '[ServiceExtensionDemo]'
+
+class MyServiceExtensionAbilityStub extends rpc.RemoteObject {
+    constructor(des) {
+        super(des)
+    }
+
+    onRemoteRequest(code, data, reply, option) {
+        console.log(printLog + "MyServiceExtensionAbility onRemoteRequest call")
+        if (code === 1) {
+            globalThis.receiveMessage = data.readString()
+            console.log(printLog + "receive message : " + globalThis.receiveMessage)
+        } else {
+            console.log(printLog + "unknown request code")
+        }
+        reply.writeString("MyServiceExtensionAbility receive your message")
+        return true
+    }
+
+    queryLocalInterface(descriptor) {
+        return null
+    }
+
+    getInterfaceDescriptor() {
+        return ''
+    }
+
+    sendRequest(code, data, reply, option) {
+        return null
+    }
+
+    getCallingPid() {
+        return 1
+    }
+
+    getCallingUid() {
+        return 1
+    }
+
+    attachLocalInterface(localInterface, descriptor) {}
+}
+
+export default class MyServiceExtensionAbility extends ServiceExtensionAbility {
+    onCreate(want) {
+        console.log(printLog + 'MyServiceExtensionAbility OnCreate, want : ' + want.abilityName)
+    }
+    onRequest(want) {
+        console.log(printLog + 'MyServiceExtensionAbility onRequest, want : ' + want.abilityName)
+    }
+    onConnect(want) {
+        console.log(printLog + 'MyServiceExtensionAbility onConnect, want : ' + want.abilityName)
+        return new MyServiceExtensionAbilityStub('test')
+    }
+    onDisconnect(want) {
+        console.log(printLog + 'MyServiceExtensionAbility onDisconnect, want : ' + want.abilityName)
+    }
+    onDestroy() {
+        console.log(printLog + 'MyServiceExtensionAbility onDestroy')
+    }
+}
