0cc6d2f6bf34c1d3e189dbb78785e8474ef6839a
wangkailong <wangkailong6@huawei.com>
2023-04-06 20:29:05 +0800

    需求
    Signed-off-by: wangkailong <wangkailong6@huawei.com>
    Change-Id: If00e91d6a5bbd2cb3304bfab718286227ca328d4

--- services/dtbschedmgr/test/unittest/mock_form_mgr_service.h
+++ services/dtbschedmgr/test/unittest/mock_form_mgr_service.h
@@ -153,6 +153,11 @@ public:
     {
         return ERR_OK;
     };
+    int32_t AcquireFormData(int64_t formId, int64_t requestCode, const sptr<IRemoteObject> &callerToken,
+        AAFwk::WantParams &formData) override
+    {
+        return ERR_OK;
+    };
     int32_t RecvFormShareInfoFromRemote(const FormShareInfo &info) override
     {
         return ERR_OK;
