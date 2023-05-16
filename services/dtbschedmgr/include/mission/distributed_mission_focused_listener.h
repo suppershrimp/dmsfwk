7582bf0465c75a0485f3d0305ff9bc25c13903ef
liuxiaowei <liuxiaowei42@huawei.com>
2023-05-16 11:17:35 +0800

    add focused and unfocused
    issue:https://gitee.com/openharmony/ability_dmsfwk/issues/I73FD0
    
    Signed-off-by: liuxiaowei <liuxiaowei42@huawei.com>

---
+++ services/dtbschedmgr/include/mission/distributed_mission_focused_listener.h
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_DISTRIBUTED_MISSION_FOCUSED_LISTENER_H
+#define OHOS_DISTRIBUTED_MISSION_FOCUSED_LISTENER_H
+
+#include "mission_listener_stub.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+class DistributedMissionFocusedListener : public AAFwk::MissionListenerStub {
+public:
+    DistributedMissionFocusedListener() = default;
+    virtual ~DistributedMissionFocusedListener() = default;
+
+    /**
+     * @brief When a mission is created, AbilityMs notifies the listener of the mission id
+     *
+     * @param missionId, mission Id.
+     */
+    void OnMissionCreated(int32_t missionId) override;
+
+    /**
+     * @brief When a mission is destroyed, AbilityMs notifies the listener of the mission id
+     *
+     * @param missionId, mission Id.
+     */
+    void OnMissionDestroyed(int32_t missionId) override;
+
+    /**
+     * @brief When the snapshot of a mission changes, AbilityMs notifies the listener of the mission id
+     *
+     * @param missionId, mission Id.
+     */
+    void OnMissionSnapshotChanged(int32_t missionId) override;
+
+    /**
+     * @brief When a mission is moved to front, AbilityMs notifies the listener of the mission id
+     *
+     * @param missionId, mission Id.
+     */
+    void OnMissionMovedToFront(int32_t missionId) override;
+
+    /**
+     * @brief When a mission get focused, AbilityMs notifies the listener of the mission id
+     *
+     * @param missionId, mission Id.
+     */
+    void OnMissionFocused(int32_t missionId) override;
+
+    /**
+     * @brief When a mission lose focused, AbilityMs notifies the listener of the mission id
+     *
+     * @param missionId, mission Id.
+     */
+    void OnMissionUnfocused(int32_t missionId) override;
+
+#ifdef SUPPORT_DISTRIBUTED_MISSION_MANAGER
+    /**
+     * @brief Called when a mission has changed it's icon.
+     *
+     * @param missionId, mission Id.
+     * @param icon, mission icon.
+     */
+    void OnMissionIconUpdated(int32_t missionId, const std::shared_ptr<OHOS::Media::PixelMap> &icon) override;
+#endif
+
+    /**
+     * @brief When a mission is closed, AbilityMs notifies the listener of the mission id
+     *
+     * @param missionId, mission Id.
+     */
+    virtual void OnMissionClosed(int32_t missionId) override;
+
+    /**
+     * @brief When a mission's label changed, AbilityMs notifies the listener of the mission id
+     *
+     * @param missionId, mission Id.
+     */
+    virtual void OnMissionLabelUpdated(int32_t missionId) override;
+};
+} // namespace DistributedSchedule
+} // namespace OHOS
+#endif // OHOS_DISTRIBUTED_MISSION_FOCUSED_LISTENER_H
