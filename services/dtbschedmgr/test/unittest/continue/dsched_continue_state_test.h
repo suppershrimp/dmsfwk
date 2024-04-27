5e92f174a8a7ebb7a45138afde789b615776d8b1
师皓杰 <shihaojie10@huawei.com>
2024-04-27 08:10:48 +0000

    update services/dtbschedmgr/test/unittest/continue/dsched_continue_state_test.h.
    Signed-off-by: 师皓杰 <shihaojie10@huawei.com>

--- services/dtbschedmgr/test/unittest/continue/dsched_continue_state_test.h
+++ services/dtbschedmgr/test/unittest/continue/dsched_continue_state_test.h
@@ -12,3 +12,114 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#ifndef DSCHED_CONTINUE_STATE_TEST_H
+#define DSCHED_CONTINUE_STATE_TEST_H
+
+#include "gtest/gtest.h"
+
+#include "dsched_continue_data_state.h"
+#include "dsched_continue_sink_end_state.h"
+#include "dsched_continue_sink_start_state.h"
+#include "dsched_continue_sink_wait_end_state.h"
+#include "dsched_continue_ability_state.h"
+#include "dsched_continue_source_end_state.h"
+#include "dsched_continue_source_start_state.h"
+#include "dsched_continue_source_wait_end_state.h"
+#include "dsched_continue_state_machine.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+
+class DSchedContinueDataStateTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueDataState> dataStateTest_;
+};
+
+class DSchedContinueSinkEndStateTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueSinkEndState> sinkEndStateTest_;
+};
+
+class DSchedContinueSinkStartStateTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueSinkStartState> sinkStartStateTest_;
+};
+
+class DSchedContinueSinkWaitEndStateTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueSinkWaitEndState> sinkWaitEndTest_;
+};
+
+class DSchedContinueAbilityStateTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueAbilityState> abilityStateTest_;
+};
+
+class DSchedContinueEndStateTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueEndState> srcEndStateTest_;
+};
+
+class DSchedContinueSourceStartStateTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueSourceStartState> srcStartStateTest_;
+};
+
+class DSchedContinueWaitEndStateTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueWaitEndState> srcWaitEndTest_;
+};
+
+class DSchedContinueStateMachineTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    std::shared_ptr<DSchedContinue> CreateObject();
+    std::shared_ptr<DSchedContinueStateMachine> stateMachineTest_;
+};
+} // namespace DistributedSchedule
+} // namespace OHOS
+#endif // DSCHED_CONTINUE_STATE_TEST_H
