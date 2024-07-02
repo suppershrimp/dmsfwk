c6f6b2fd2e0b26563a89858ab68f1784dd5d214d
dengxiaoyu <dengxiaoyu6@huawei.com>
2024-07-02 16:33:19 +0800

    del unfocused switch control
    Signed-off-by: dengxiaoyu <dengxiaoyu6@huawei.com>

--- services/dtbschedmgr/src/mission/dms_continue_send_manager.cpp
+++ services/dtbschedmgr/src/mission/dms_continue_send_manager.cpp
@@ -710,13 +710,6 @@ int32_t DMSContinueSendMgr::GetAccessTokenIdSendEvent(std::string bundleName,
     }
 
     if (screenOffHandler_->IsDeviceScreenOn()) {
-        bool IsContinueSwitchOn = SwitchStatusDependency::GetInstance().IsContinueSwitchOn();
-        HILOGI("IsContinueSwitchOn : %{public}d", IsContinueSwitchOn);
-        if (!IsContinueSwitchOn) {
-            HILOGE("ContinueSwitch status is off");
-            return DMS_PERMISSION_DENIED;
-        }
-
         ret = SendSoftbusEvent(bundleNameId, continueTypeId, DMS_UNFOCUSED_TYPE);
         bool res = (reason != UnfocusedReason::TIMEOUT)
             ? DmsRadar::GetInstance().NormalUnfocusedSendEventRes("SendSoftbusEvent", ret)
