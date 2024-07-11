1551ca7e766648ace363d29fc4085b60bd8c57e6
dengxiaoyu <dengxiaoyu6@huawei.com>
2024-07-11 12:18:23 +0800

    add vector empty check
    Signed-off-by: dengxiaoyu <dengxiaoyu6@huawei.com>

--- services/dtbschedmgr/src/continue/dsched_continue.cpp
+++ services/dtbschedmgr/src/continue/dsched_continue.cpp
@@ -1110,8 +1110,10 @@ void DSchedContinue::DurationDumperComplete(int32_t result)
         std::string strEndTime = DmsContinueTime::GetInstance().GetCurrentTime();
         DmsContinueTime::GetInstance().SetDurationEnd(CONTINUE_TOTAL_TIME, GetTickCount());
         DmsContinueTime::GetInstance().SetDurationStrTime(CONTINUE_END_TIME, strEndTime);
+
         DmsUE::GetInstance().DmsContinueComplete(continueInfo_.sourceBundleName_, continueInfo_.sinkAbilityName_,
-        continueInfo_.sourceDeviceId_, result);
+            continueInfo_.sourceDeviceId_, result);
+
         DmsContinueTime::GetInstance().AppendInfo();
         DmsContinueTime::GetInstance().SetPull(false);
     }
