3fc72998099820d2f80c9945015f2443fa1ecbf1
师皓杰 <shihaojie10@huawei.com>
2024-07-27 16:15:50 +0800

    修改失败用例
    Signed-off-by: 师皓杰 <shihaojie10@huawei.com>

--- services/dtbschedmgr/test/unittest/distributed_event/dms_client_test.h
+++ services/dtbschedmgr/test/unittest/distributed_event/dms_client_test.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,44 +13,40 @@
  * limitations under the License.
  */
 
-#ifndef OHOS_DMS_HANDLER_TEST_H
-#define OHOS_DMS_HANDLER_TEST_H
+#ifndef OHOS_DMS_CLIENT_TEST_H
+#define OHOS_DMS_CLIENT_TEST_H
 
-#include "dms_handler.h"
+#include "dms_client.h"
 
-#include <condition_variable>
-#include <map>
-#include <mutex>
 #include <string>
-#include <vector>
 
+#include "ability_manager_errors.h"
 #include "distributed_event_listener.h"
-#include "dms_sa_client.h"
 #include "dms_sdk_demo.h"
 #include "gtest/gtest.h"
-#include "refbase.h"
-#include "single_instance.h"
+#include "iremote_broker.h"
+
 
 namespace OHOS {
 namespace DistributedSchedule {
-class DmsHandlerTest : public testing::Test {
+class DistributedClientTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
     void SetUp();
     void TearDown();
-    void DSchedEventNotify(EventNotify &notify);
 
-    DmsHandler dmsHandler_;
+protected:
+    DistributedClient distributedClient_;
 };
 
-class BusinessTest : public DSchedEventListenerStub {
+class BusinessHandlerTest : public DSchedEventListenerStub {
 public:
-    BusinessTest() = default;
-    ~BusinessTest() = default;
+    BusinessHandlerTest() = default;
+    ~BusinessHandlerTest() = default;
 
     void DSchedEventNotify(EventNotify &notify);
 };
 } // namespace DistributedSchedule
 } // namespace OHOS
-#endif // OHOS_DMS_HANDLER_TEST_H
+#endif // OHOS_DMS_CLIENT_TEST_H
