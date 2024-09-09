2df18453396fb02d6e3535a0a041016a341b1f6e
zhubingwei <zhubingwei@huawei.com>
2024-09-09 14:00:07 +0800

    长按添加桌面卡片，闪现问题，等待1秒后才能正确刷新
    Signed-off-by: zhubingwei <zhubingwei@huawei.com>

--- services/dtbschedmgr/test/unittest/mock_form_mgr_service.h
+++ services/dtbschedmgr/test/unittest/mock_form_mgr_service.h
@@ -253,6 +253,12 @@ public:
     {
         return ERR_OK;
     };
+    ErrCode RequestPublishProxyFormWithSnapshot(Want &want, bool withFormBindingData,
+        std::unique_ptr<FormProviderData> &formBindingData, int64_t &formId,
+        const std::vector<FormDataProxy> &formDataProxies) override
+    {
+        return ERR_OK;
+    }
 };
 }  // namespace AppExecFwk
 }  // namespace OHOS
