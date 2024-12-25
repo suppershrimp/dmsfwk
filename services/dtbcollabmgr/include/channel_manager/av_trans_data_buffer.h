b0ba401a43085946beb5ec17feeab44ee18cb5fc
baishunqin <baishunqin@huawei.com>
2024-12-25 10:46:05 +0800

    增加ChannelManager的数据基础收发功能
    Signed-off-by: baishunqin <baishunqin@huawei.com>

---
+++ services/dtbcollabmgr/include/channel_manager/av_trans_data_buffer.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_AV_TRANS_DATA_BUFFER_H
+#define OHOS_AV_TRANS_DATA_BUFFER_H
+
+#include <string>
+
+namespace OHOS {
+namespace DistributedCollab {
+class AVTransDataBuffer {
+public:
+    explicit AVTransDataBuffer(size_t capacity);
+    ~AVTransDataBuffer();
+
+    size_t Size();
+    size_t Offset();
+    size_t Capacity();
+    uint8_t *Data();
+    int32_t SetRange(size_t offset, size_t size);
+
+private:
+    static const uint32_t DSCHED_MAX_BUFFER_SIZE = 80 * 1024 * 1024;
+
+    size_t capacity_ = 0;
+    size_t rangeOffset_ = 0;
+    size_t rangeLength_ = 0;
+    uint8_t *data_ = nullptr;
+};
+}  // namespace DistributedSchedule
+}  // namespace OHOS
+#endif  // OHOS_AV_TRANS_DATA_BUFFER_H
---
+++ services/dtbcollabmgr/include/channel_manager/channel_common_definition.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_DSCHED_COLLAB_CHANNEL_COMMON_DEFINITION_H
+#define OHOS_DSCHED_COLLAB_CHANNEL_COMMON_DEFINITION_H
+#include "data_sender_receiver.h"
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace OHOS {
+namespace DistributedCollab {
+    enum class ChannelStatus {
+        CONNECTED,
+        UNCONNECTED
+    };
+
+    enum class ChannelDataType {
+        MESSAGE = 0,
+        BYTES,
+        VIDEO_STREAM
+    };
+
+    struct ChannelPeerInfo {
+        std::string peerName;
+        std::string networkId;
+    };
+
+    struct ChannelInfo {
+        int32_t channelId;
+        ChannelStatus status;
+        ChannelDataType dataType;
+        std::string channelName;
+        ChannelPeerInfo peerInfo;
+        std::vector<int32_t> clientSockets;
+        // socketId->sender
+        std::map<int32_t, std::unique_ptr<DataSenderReceiver>> dataSenderReceivers;
+    };
+}
+}
+#endif
