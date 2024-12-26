e6abdd05bdacc2336074373640752c7d2fbf350c
m30043719 <maxiaodong25@huawei.com>
2024-12-26 16:03:56 +0800

    协同状态机开发
    Signed-off-by: m30043719 <maxiaodong25@huawei.com>

---
+++ services/dtbschedmgr/src/collab/state/sink_state/dsched_collab_sink_connect_state.cpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dsched_collab_sink_connect_state.h"
+
+#include "dsched_collab.h"
+#include "dsched_collab_event.h"
+#include "dtbschedmgr_log.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+namespace {
+const std::string TAG = "CollabSinkConnectState";
+}
+CollabSinkConnectState::CollabSinkConnectState(std::shared_ptr<DSchedCollabStateMachine> stateMachine)
+    : stateMachine_(stateMachine)
+{
+    memberFuncMap_[NOTIFY_PREPARE_RESULT_EVENT] = &CollabSinkConnectState::DoSinkPrepareResult;
+    memberFuncMap_[ERR_END_EVENT] = &CollabSinkConnectState::DoConnectError;
+}
+
+CollabSinkConnectState::~CollabSinkConnectState()
+{
+}
+
+int32_t CollabSinkConnectState::Execute(std::shared_ptr<DSchedCollab> dCollab,
+    const AppExecFwk::InnerEvent::Pointer &event)
+{
+    if (event == nullptr) {
+        HILOGE("event is null");
+        return INVALID_PARAMETERS_ERR;
+    }
+    auto iterFunc = memberFuncMap_.find(event->GetInnerEventId());
+    if (iterFunc == memberFuncMap_.end()) {
+        HILOGE("execute %{public}d in wrong state", event->GetInnerEventId());
+        return COLLAB_STATE_MACHINE_INVALID_STATE;
+    }
+
+    auto memberFunc = iterFunc->second;
+    int32_t ret = (this->*memberFunc)(dCollab, event);
+    if (ret != ERR_OK) {
+        HILOGI("execute %{public}d failed, ret: %{public}d", event->GetInnerEventId(), ret);
+    }
+    return ret;
+}
+
+CollabStateType CollabSinkConnectState::GetStateType()
+{
+    return SINK_CONNECT_STATE;
+}
+
+int32_t CollabSinkConnectState::DoSinkPrepareResult(std::shared_ptr<DSchedCollab> dCollab,
+    const AppExecFwk::InnerEvent::Pointer &event)
+{
+    if (dCollab == nullptr || event == nullptr) {
+        HILOGE("dCollab or event is null");
+        return INVALID_PARAMETERS_ERR;
+    }
+    auto syncCollabData = event->GetSharedObject<int32_t>();
+    int32_t ret = dCollab->ExeSinkPrepareResult(*syncCollabData);
+    if (ret != ERR_OK) {
+        HILOGE("failed, ret: %{public}d", ret);
+    }
+    return ret;
+}
+
+int32_t CollabSinkConnectState::DoConnectError(std::shared_ptr<DSchedCollab> dCollab,
+    const AppExecFwk::InnerEvent::Pointer &event)
+{
+    if (dCollab == nullptr || event == nullptr) {
+        HILOGE("dCollab or event is null");
+        return INVALID_PARAMETERS_ERR;
+    }
+    auto syncCollabData = event->GetSharedObject<int32_t>();
+    int32_t ret = dCollab->ExeSinkConnectError(*syncCollabData);
+    if (ret != ERR_OK) {
+        HILOGE("failed, ret: %{public}d", ret);
+    }
+    return ret;
+}
+}  // namespace DistributedSchedule
+}  // namespace OHOS
