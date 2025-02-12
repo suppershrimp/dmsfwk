de3aaf8ea1120fe32083121d916c9f1d9256c1da
MisterE <smart_e@126.com>
2025-02-12 16:26:46 +0800

    服务初始化优化
    Signed-off-by: MisterE <smart_e@126.com>

--- services/dtbschedmgr/src/continue/dsched_continue_manager.cpp
+++ services/dtbschedmgr/src/continue/dsched_continue_manager.cpp
@@ -54,6 +54,10 @@ DSchedContinueManager::~DSchedContinueManager()
 void DSchedContinueManager::Init()
 {
     HILOGI("Init DSchedContinueManager start");
+    if(hasInit_.load()){
+        HILOGW("Init DSchedContinueManager has init");
+        return;
+    }
     if (eventHandler_ != nullptr) {
         HILOGI("DSchedContinueManager already inited, end.");
         return;
@@ -86,6 +90,7 @@ void DSchedContinueManager::StartEvent()
 void DSchedContinueManager::UnInit()
 {
     HILOGI("UnInit start");
+    hasInit_.store(false);
     DSchedTransportSoftbusAdapter::GetInstance().UnregisterListener(SERVICE_TYPE_CONTINUE, softbusListener_);
     DSchedTransportSoftbusAdapter::GetInstance().ReleaseChannel();
     continues_.clear();
